876cffdd6323b5247f2313c07738d983bce9577e
Maclyn Afonso <>
2020-12-24 20:18:52 +0000

    applied styling to TODO component

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,22 +1,30 @@
-import { Button, FormControl, Input } from '@material-ui/core';
 import React, { useState } from 'react';
+import { Button, Input } from '@material-ui/core';
 import './App.css';
 import Todo from './Todo';
 
 function App() {
   // Setup state for list of todos using 'useState' Hook -> short term memory
   const [todos, setTodos] = useState([])
-  // Setup state for the input field
-  const [input, setInput] = useState('')
+  // Setup state for the title field
+  const [titleInput, setTitleInput] = useState('')
+  // Setup state for the description field
+  const [descriptionInput, setDescriptionInput] = useState('')
 
   // Setup onclick add event
   const addTodo = (event) => {
     // Prevent page from refreshing when form is submitted
     event.preventDefault()
     // execute this part of code when 'SAVE' button is clicked
-    setTodos([...todos, input])
+    setTodos([
+        ...todos, 
+        {
+          'title': titleInput,
+          'description': descriptionInput
+      }])
     // clear input after submitting
-    setInput('')
+    setTitleInput('')
+    setDescriptionInput('')
   }
 
   // Stuff to render
@@ -24,23 +32,20 @@ function App() {
     <div className="App">
       <h1>Plan your to-do's...</h1>
       <h2>Lets get started 🚀</h2>
-      <form>
-        <FormControl>
-          <Input value={input} onChange={event => setInput(event.target.value)} />
-        </FormControl>
-        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={addTodo}>
+      <form className='TodoForm'>
+        <Input value={titleInput} onChange={event => setTitleInput(event.target.value)} placeholder='Title'/>
+        <Input value={descriptionInput} onChange={event => setDescriptionInput(event.target.value)} placeholder='Description' className='DescriptionInput' />
+        <Button disabled={!titleInput} variant="contained" color="primary" type="submit" onClick={addTodo}>
           {/* { console.log(input.toString()) } */}
           Save
         </Button>
       </form>
       <div>
-        <ul>
-          {/* Loop through the todos set in useState */}
-          {todos.map((todo, index) => (
-            <Todo todo={todo} key={index} />
-          ))
-          }
-        </ul>
+        {/* Loop through the todos set in useState */}
+        {todos.map((todo, index) => (
+          <Todo title={todo.title} description={todo.description} key={index} />
+        ))
+        }
       </div>
     </div>
   )
--- todo_app/src/Todo.js
+++ todo_app/src/Todo.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Checkbox, IconButton, List, ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
+import './Todo.css'
 
 function Todo(props) {
     return (
-        <div>
-            <li>
-                {props.todo}
-            </li>
+        <div className="Todo">
+            <List>
+                <ListItem button>
+                <ListItemIcon>
+                    <Checkbox edge="start" />
+                </ListItemIcon>
+                <ListItemText primary={props.title} secondary={props.description}/>
+                <ListItemSecondaryAction>
+                    <IconButton edge="end">
+                        <DeleteIcon />
+                    </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+            </List>
         </div>
     )
 }
